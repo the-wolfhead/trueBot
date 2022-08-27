8113b1dde215f301e2bb10f543d7720b602f78ab
Daniel Otabor <danieldamianotabor@gmail.com>
2022-08-27 08:23:44 +0100

    ts02

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, first_question_step))
 
     # log all errors
     dp.add_error_handler(error)
