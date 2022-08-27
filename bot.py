d90e73f5ed57ab322a0f0b3f79d14b5eb5c814e0
Daniel Otabor <danieldamianotabor@gmail.com>
2022-08-27 08:51:42 +0100

    tst5

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("5751562706:AAHP1bVDHWWV_2smAceLajMp-UI6ApIPTIE", use_context=True)
+    bot = telebot.TeleBot(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
