cb51d2d8d1f61a132f6685d759c902eab2de48f2
Daniel Otabor <danieldamianotabor@gmail.com>
2022-08-27 12:01:42 +0100

    tst19

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 from telegram import Bot
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import telebot
 from telebot import types
 import os
