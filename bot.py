e3d546844e70dd48033268aee8bf3dbc2dbd74aa
Daniel Otabor <danieldamianotabor@gmail.com>
2022-08-27 12:06:48 +0100

    tst22

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ ONE , TWO = range(2)
 
 
 def start(update, context: CallbackContext):
-     bot.send_message(chat_id , text = "Hi, how are you doing and how may I help you ?")
+     update.send_message(chat_id , text = "Hi, how are you doing and how may I help you ?")
      return ONE
 
 def ask_account(update, context: CallbackContext):
